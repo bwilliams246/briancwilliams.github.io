7f4c31f06985fdaeb3f07da4c71716b5e9795d1c
Brian <bwilliams7622@gmail.com>
2022-10-08 15:24:10 -0500

    desktop and mobile: JavaScript animations implemented

--- Blogr/js/app.js
+++ Blogr/js/app.js
@@ -1,26 +1,47 @@
 /* DESKTOP LOGIC */
-    /* GLOBAL VARIABLES */
 const navOptions = [...document.getElementsByClassName('sub-container')];
 const navInfo = [...document.getElementsByClassName('extra-nav-info')];
+const columnSections = [...document.getElementsByClassName('two-columns')];
 
-    /* EVENT LISTENERS */
 navOptions.forEach((link , i) => link.addEventListener('click' , e => {
     navInfo[i].classList.toggle('hide');
 }));
-
-    /* FUNCTIONS */
 
 /* MOBILE LOGIC */
 const hamburgerBtn = document.getElementsByClassName('hamburger-menu')[0]
 const mobileMenu = document.getElementsByClassName('mobile-menu-container')[0];
 const mobileNavLinks = [...document.getElementsByClassName('mobile-sub-container')];
 const additionalNavInfo = [...document.getElementsByClassName('mobile-extra-nav-info')];
-let mobileLinkText = [];
 
+/* Opens / displays the mobile menu on click */
 hamburgerBtn.addEventListener('click' , e => mobileMenu.classList.toggle('hide-mobile'));
-mobileNavLinks.forEach( (link , i) => {
-    mobileLinkText.push(link.childNodes[1]);
-    link.childNodes[1].addEventListener('click' , e => {
+
+/* Loops through each link container, adds a click listener to the 
+   <p> child node, and displays the additional navbar links
+*/
+mobileNavLinks.forEach( (link , i) => link.childNodes[1].addEventListener('click' , e => {
         additionalNavInfo[i].classList.toggle('hide-extra-info');
     })
+);
+
+/* ANIMATIONS SECTION */
+const showText = textArr => {
+    textArr.forEach( elem => {
+        if (elem.isIntersecting) {
+            elem.target.classList.add('visible');
+        }
+        else elem.target.classList.remove('visible');
+    });
+};
+
+let options = {
+    rootMargin: '-5%',
+    threshold: .1
+};
+
+let observer = new IntersectionObserver(showText , options);
+
+console.log(columnSections);
+columnSections.forEach( section => {
+    observer.observe(section);
 });
